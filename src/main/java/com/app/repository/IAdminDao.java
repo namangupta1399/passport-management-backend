5ddb8fc5aaf85d2080cbd5e690f3f31859d2b2bf
Aman Verma <vermaaman1998@gmail.com>
2021-04-20 13:23:46 +0530

    Service Layer updated

--- src/main/java/com/app/repository/IAdminDao.java
+++ src/main/java/com/app/repository/IAdminDao.java
@@ -1,23 +1,5 @@
 package com.app.repository;
 
 public interface IAdminDao {
-	void addAdmin(Admin admin);
-
-	void deleteAdmin(Admin admin);
-
-	void updateAdmin(int adminId, Admin admin);
-
-	Admin viewAdmin(int adminId);
 	
-	void addPassportApplication(Admin admin);
-	
-	void deletePassportApplication(Admin admin);
-	
-	void updatePassportApplication(int applicantId, Admin admin);
-	
-	Applicant viewPassportApplication(int applicantId);
-	
-	void addHelpDeskQuery(HelpDeskQuery helpDeskQuery);
-	
-	void updateHelpDeskQuery(int applicantId, HelpDeskQuery helpDeskQuery);
 }
---
+++ src/main/java/com/app/service/ApplicantServiceImpl.java
@@ -0,0 +1,65 @@
+package com.app.service;
+
+public class ApplicantServiceImpl implements IApplicantService{
+
+	@Override
+	public void addApplicant(Applicant applicant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteApplicant(Applicant applicant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateApplicant(int applicantId, Applicant applicant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Applicant viewApplicant(int applicantId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPassportApplication(Application application) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletePassportApplication(Application application) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePassportApplication(int applicantId, Application application) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Applicant viewPassportApplication(int applicantId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addHelpDeskQuery(HelpDeskQuery helpDeskQuery) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateHelpDeskQuery(int applicantId, HelpDeskQuery helpDeskQuery) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
