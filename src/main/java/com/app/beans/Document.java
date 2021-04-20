58d0dded997d7b44c4e6b02ba2f70485e8ec60a0
Simran <simran12414@gmail.com>
2021-04-20 13:05:56 +0530

    Created POJO for document and Helpdesk

---
+++ src/main/java/com/app/beans/Document.java
@@ -0,0 +1,94 @@
+package com.app.beans;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="documents")
+public class Document {
+	@Id
+	@GeneratedValue
+	private int documentId; 
+	private String documentName;
+	private String documentValue;
+	private boolean isVerified;
+	private Date addedOn;
+	private Date updatedOn;
+	
+	public Document() {
+		super();
+	}
+
+	public Document(int documentId, String documentName, String documentValue, Boolean isVerified, Date addedOn,
+			Date updatedOn) {
+		super();
+		this.documentId = documentId;
+		this.documentName = documentName;
+		this.documentValue = documentValue;
+		this.isVerified = isVerified;
+		this.addedOn = addedOn;
+		this.updatedOn = updatedOn;
+	}
+
+	public int getDocumentId() {
+		return documentId;
+	}
+
+	public void setDocumentId(int documentId) {
+		this.documentId = documentId;
+	}
+
+	public String getDocumentName() {
+		return documentName;
+	}
+
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+
+	public String getDocumentValue() {
+		return documentValue;
+	}
+
+	public void setDocumentValue(String documentValue) {
+		this.documentValue = documentValue;
+	}
+
+	public boolean getIsVerified() {
+		return isVerified;
+	}
+
+	public void setIsVerified(boolean isVerified) {
+		this.isVerified = isVerified;
+	}
+
+	public Date getAddedOn() {
+		return addedOn;
+	}
+
+	public void setAddedOn(Date addedOn) {
+		this.addedOn = addedOn;
+	}
+
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+	@Override
+	public String toString() {
+		return "Document [documentId=" + documentId + ", documentName=" + documentName + ", documentValue=" + documentValue
+				+ ", isVerified=" + isVerified + ", addedOn=" + addedOn + ", updatedOn=" + updatedOn + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/app/beans/Helpdesk.java
@@ -0,0 +1,92 @@
+package com.app.beans;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="helpdesk")
+public class Helpdesk {
+
+	@Id
+	@GeneratedValue
+	private int helpdeskId;
+	private String query;
+	private String solution;
+	private boolean isResolved;
+	private Date createdOn;
+	private Date updatedOn;
+	
+	public Helpdesk() {
+		super();
+		
+	}
+
+	public Helpdesk(int helpdeskId, String query, String solution, boolean isResolved, Date createdOn, Date updatedOn) {
+		super();
+		this.helpdeskId = helpdeskId;
+		this.query = query;
+		this.solution = solution;
+		this.isResolved = isResolved;
+		this.createdOn = createdOn;
+		this.updatedOn = updatedOn;
+	}
+
+	public int getHelpdeskId() {
+		return helpdeskId;
+	}
+
+	public void setHelpdeskId(int helpdeskId) {
+		this.helpdeskId = helpdeskId;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public String getSolution() {
+		return solution;
+	}
+
+	public void setSolution(String solution) {
+		this.solution = solution;
+	}
+
+	public boolean isResolved() {
+		return isResolved;
+	}
+
+	public void setResolved(boolean isResolved) {
+		this.isResolved = isResolved;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+	@Override
+	public String toString() {
+		return "Helpdesk [helpdeskId=" + helpdeskId + ", query=" + query + ", solution=" + solution + ", isResolved="
+				+ isResolved + ", createdOn=" + createdOn + ", updatedOn=" + updatedOn + "]";
+	}
+	
+}
